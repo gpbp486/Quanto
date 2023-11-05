a508579887b92721e74ddab070803a3194bdfbc6
gpbp486 <102256050+gpbp486@users.noreply.github.com>
2023-11-05 10:36:50 -0500

    General debugging

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
 from flask import Flask, render_template
+import os
+import openai
+
 
 app = Flask(__name__)
 
+
+OPENAI_API_KEY = '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'
+
+
+openai.api_key = OPENAI_API_KEY
+
+
+mess = [{'role':'system', 'content':'You are a helpful assistant on a website for children. The website teaches them the basics of Quantum Computing\n'
+	            	'You have vast knowledge of quantum physics and quantum computing\n'
+	            	'Your job is to answer questions related to quantum computation, your name is Quanto.\n'
+	                'You are very well-spoken and often make jokes appropriate for children\n'
+                    'Try to avoid using large words, however if necessary make sure to define them as they occur.\n'
+	            	'You are polite and eager to help, and keep your answers short and easy to understand, answers must be simple enough that a 5th grader can understand!\n'}]
+
+
+
+def get_response(question):
+
+    #Update Message List with each Dialoge so that our Bot can gain insight and the conversation flows
+    mess.append({'role':'assistant', 'content': response.choices[0].message.content})
+    mess.append({'role': 'user', 'content': question})
+    response =  openai.ChatCompletion.create(model='gpt-3.5-turbo', messages = mess, temperature = 0.5, max_tokens = 250)
+    return str(response.choices[0].message.content)
+
+
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # You can pass the initial message or any other data you want to send to the template here
+    initial_message = "Welcome to Quanto Chatbot. How can I help you?"
+    return render_template('index.html', initial_message=initial_message)
 
+
+
+@app.route('/<message_input>')
+def query(message_input):
+    post = get_response(message_input)
+    return render_template('answer.html', answer=post)
---
+++ static/index.js
@@ -0,0 +1,59 @@
+const chatMessages = document.getElementById("chat-messages");
+const messageInput = document.getElementById("message_input");
+const sendButton = document.getElementById("send-button");
+const quanto = document.getElementById("quanto-message");
+const initialMessage = document.querySelector(".message"); // Find the message element in the DOM
+
+// Auto-scroll chat to bottom
+function scrollToBottom() {
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+// Display the initial message
+if (initialMessage) {
+    const initialMessageText = initialMessage.textContent;
+    const initialMessageElement = document.createElement("div");
+    initialMessageElement.textContent = "Quanto: " + initialMessageText;
+    chatMessages.appendChild(initialMessageElement);
+}
+
+
+messageInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        sendButton.click();
+    }
+});
+
+
+
+sendButton.addEventListener("click", async () => {
+    const userMessage = messageInput.value.trim();
+    if (userMessage !== "") {
+        // User message
+        const userMessageElement = document.createElement("div");
+        userMessageElement.textContent = "You: " + userMessage;
+        chatMessages.appendChild(userMessageElement);
+
+        // Send the user's message to your server for processing by OpenAI
+        const response = await sendMessageToServer(userMessage);
+
+        // Response message
+        const responseMessageElement = document.createElement("div");
+        responseMessageElement.textContent = "Quanto: " + response;
+        chatMessages.appendChild(responseMessageElement);
+
+        messageInput.value = "";
+        scrollToBottom(); // Auto-scroll when new message added
+    }
+});
+
+// Define a function to send user's message to the server
+async function sendMessageToServer(userMessage) {
+    const response = await fetch('/' + userMessage);
+    const data = await response.json();
+    return data.answer;
+}
+
+
+// Initial auto-scroll to the bottom
+scrollToBottom();
